024b7ef09419ee999d0593493630ece5f3ba2c8e
Welligton Rios <wriosfaculdade@gmail.com>
2020-07-05 10:37:05 -0300

    Comunicação Rest criada e testada

---
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -0,0 +1,20 @@
+package com.example.demo.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categoria")
+public class CategoriaResource {
+
+	@GetMapping
+	public String listar() {
+		return "Ola mundo, rest funcionando";
+	}
+	
+	
+	
+	
+	
+}
