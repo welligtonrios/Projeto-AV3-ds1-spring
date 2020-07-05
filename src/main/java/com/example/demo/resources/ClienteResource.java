ac497395fb6ed94c21fbc316acea718d70b2d425
Welligton Rios <wriosfaculdade@gmail.com>
2020-07-05 17:51:46 -0300

    Criando e testando Rest Service Cliente

--- src/main/java/com/example/demo/resources/ClienteResource.java
+++ src/main/java/com/example/demo/resources/ClienteResource.java
@@ -7,22 +7,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-import com.example.demo.domain.Cliente;
-
-import com.example.demo.service.ClienteService;
+import com.example.demo.domain.Categoria;
+import com.example.demo.service.CategoriaService;
 
 @RestController
-@RequestMapping(value = "/cliente")
-public class CategoriaResource {
+@RequestMapping(value = "/categoria")
+public class ClienteResource {
 
 	@Autowired
-	private ClienteService service;
+	private CategoriaService service;
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 
-		Cliente obj = service.find(id);
+		Categoria obj = service.find(id);
 
 		return ResponseEntity.ok().body(obj);
 	}
---
+++ src/main/java/com/example/demo/service/ClienteService.java
@@ -0,0 +1,34 @@
+package com.example.demo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.Categoria;
+import com.example.demo.domain.Cliente;
+import com.example.demo.repositories.CategoriaRepository;
+import com.example.demo.repositories.ClienteRepository;
+import com.example.demo.service.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repository;
+	
+	
+	public Cliente find(Integer id) {
+		
+		//chamando metodo para recuperar o id do cliente 
+		Optional<Cliente> obj = repository.findById(id);
+		//quando nao existir o que eu procuro eu retorno o orElseThrow com o Objeto ObjectNotFoundException criado
+		return obj.orElseThrow((()-> new ObjectNotFoundException(
+				"Objeto não encontrado!  ID: " + id + "Tipo: " + Cliente.class.getName())));
+	}
+	
+	
+	
+	
+}
