e1786541f17f8f70b38b854a548b47eb6ec86e70
Welligton Rios <wriosfaculdade@gmail.com>
2020-07-05 10:25:59 -0300

    Projeto Criado com suas dependencias

---
+++ src/main/java/com/example/demo/ProjetoAv3Ds1SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAv3Ds1SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAv3Ds1SpringApplication.class, args);
+	}
+
+}
